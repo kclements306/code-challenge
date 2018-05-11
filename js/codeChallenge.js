da38fb8dc7cff053c9d3796e8ea3544f0e1dc09f
Ken Clements <kclements@techtonicgroup.com>
2018-05-11 12:38:07 -0600

    Done

--- js/codeChallenge.js
+++ js/codeChallenge.js
@@ -19,16 +19,27 @@ class CodeChallenge {
 
     btnGetInputString() {
         this.inputString = $("#inputString").val();
-        console.log(inputString);
         this.processString(this.inputString);
     }
 
     processString(inputString) {
-        this.hasTen = false;
+        console.time();
         let string2 = inputString.match(/[0-9?]+/g);        // Strip out anthing that's not a number or ?
-        let string3 = string2.join();                       // Get back one string 
+        let string3 = string2.join("");                     // Get back one string 
         let string4 = string3.match(/\d\?\?\?\d/g);         // Get only the cases where input is of the form d???d
-        console.log(string4);
+        if (string4 !== null) {
+            this.hasTen = true;
+            for (let i=0; i<string4.length; ++i) {
+                if ((Number(string4[i].charAt(0)) + Number(string4[i].charAt(4))) !== 10) {
+                    this.hasTen = false;
+                    // console.log(string4[i].charAt(0));
+                    // console.log(string4[i].charAt(4));
+                    break;
+                } 
+            }
+        }
+        console.log(this.hasTen);
+        console.timeEnd();
     }
 }
 
