30173ef271bfc97011d0d040044575c9eece5aef
Ken Clements <kclements@techtonicgroup.com>
2018-05-11 11:40:04 -0600

    Ready for first test

--- js/codeChallenge.js
+++ js/codeChallenge.js
@@ -1,7 +1,33 @@
 class CodeChallenge {
 
     constructor() {
-        this.inputString;
+        this.inputString = "";
+        this.hasTen = false;
+    }
+
+    init() {
+        this._bindEvents();
+    }
+
+    /*
+        _bindEvents - binds DOM events to the method used to process the event
+            returns false when all events are bound.
+    */
+    _bindEvents() {
+        $("#btnInput").on("click", $.proxy(this.btnGetInputString, this));
+    }
+
+    btnGetInputString() {
+        this.inputString = $("#inputString").val();
+        this.processString(this.inputString);
+    }
+
+    processString(inputString) {
+        this.hasTen = false;
+        let string2 = inputString.match(/[0-9?]+/g);        // Strip out anthing that's not a number or ?
+        let string3 = string2.join();                       // Get back one string 
+        let string4 = string3.match(/\d\?\?\?\d/g);         // Get only the cases where input is of the form d???d
+        console.log(string4);
     }
 }
 
